ac3633fbb125d8a4e0363f62dde375a2e64e3f6a
Andrei <andfishka@gmail.com>
2025-02-07 14:24:32 +0300

    Add audio controls and playback functionality
    - Added `loadButton` and `volSlider` to `MainComponent` and made them visible.
    - Implemented listeners for `loadButton` and `volSlider`.
    - Set range for `volSlider` and initialized audio channels.
    - Updated `resized` method to set bounds for `loadButton`.
    - Modified `buttonClicked` to handle `loadButton` clicks and load files.
    - Enhanced `buttonClicked` to control `transportSource` with `playButton` and `stopButton`.
    - Added `prepareToPlay` to register audio formats and prepare playback.
    - Updated `getNextAudioBlock` to fetch audio data from `transportSource`.
    - Added `releaseResources` to free resources used by `transportSource`.
    - Implemented `sliderValueChanged` to adjust `transportSource` gain.
    - Added `loadURL` to load audio files from URLs.
    - Updated `MainComponent.h` to include new buttons, sliders, and member variables.
    - Declared `sliderValueChanged` and `loadURL` methods in `MainComponent.h`.
    - Included `juce::Slider::Listener` in `MainComponent` class inheritance.
    - Added member variables for audio playback management and state handling.

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,13 +2,15 @@
 
 #include <JuceHeader.h>
 
+
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
 class MainComponent  : public juce::AudioAppComponent,
-	                   public juce::Button::Listener
+	                   public juce::Button::Listener,
+	                   public juce::Slider::Listener   
 {
 public:
     //==============================================================================
@@ -22,6 +24,8 @@ public:
     /** implement Button::Listener */
     void buttonClicked(juce::Button *) override;
 
+	void sliderValueChanged(juce::Slider*) override;
+
     // AudioAppComponent methods
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
@@ -34,7 +38,27 @@ private:
 
     juce::TextButton playButton{ "PLAY" };
 	juce::TextButton stopButton{ "STOP" };
+    juce::TextButton loadButton{ "LOAD" };
+
     juce::Slider volSlider;
+
+    juce::Random rand;
+
+    double phase;
+    double dphase;
+
+    bool playing;
+
+	juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::FileChooser fChooser{ "Select a file..." };
+
+	juce::AudioTransportSource transportSource;
+
+    juce::ResamplingAudioSource resampleSource{juce::&AudioTransportSource, false, 2 }
+
+
+    void loadURL(juce::URL audioURL);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
